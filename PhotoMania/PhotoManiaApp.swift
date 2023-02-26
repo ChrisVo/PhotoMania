7e8da058436d54d6657a30207b2e1412c87f0fe3
Chris Vo <chrisanhvo@gmail.com>
2023-02-25 22:29:30 -0800

    Initial Commit

---
+++ PhotoMania/PhotoManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoManiaApp.swift
+//  PhotoMania
+//
+//  Created by Chris.Vo on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
